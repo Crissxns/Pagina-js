06e420578413aa0954ea72de8c53d3df61ddae14
MarcoPY09 <marcoperezyoc@gmail.com>
2026-08-26 09:31:48 -0600

    Confirmadcion de contraseña

--- codigo.js
+++ codigo.js
@@ -1,12 +1,15 @@
-let nombre
-function inicio(){
-    let Usuario = document.getElementById("Usuario").value;
-    let Contraseña = document.getElementById("Contraseña").value
+let nombre;
 
-    if (Contraseña == "") {
-        alert("Por favor ingrese una contraseña")
+function inicio() {
+    let Usuario = document.getElementById("Usuario").value;
+    let Contraseña = document.getElementById("Contraseña").value;
+    let Contraseñaconfir = document.getElementById("Contraseñaconfir").value;
+
+    if (Contraseña === "") {
+        alert("Por favor ingrese una contraseña");
+    } else if (Contraseña !== Contraseñaconfir) {
+        alert("Error: Las contraseñas no coinciden");
     } else {
-        alert(" ¡Gracias por registrarte! Bienvenido " + Usuario);
-        
+        alert("¡Gracias por registrarte! Bienvenido " + Usuario);
     }
 }
