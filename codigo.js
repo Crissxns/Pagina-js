c0d9869261b9743b8eb66a65d330ba4f3b59bd5f
MarcoPY09 <marcoperezyoc@gmail.com>
2026-08-26 08:55:54 -0600

    Cambios en el ingreso

--- codigo.js
+++ codigo.js
@@ -1,4 +1,12 @@
+let nombre
 function inicio(){
     let Usuario = document.getElementById("Usuario").value;
     let Contraseña = document.getElementById("Contraseña").value
+
+    if (Contraseña == "") {
+        alert("Por favor ingrese una contraseña")
+    } else {
+        alert(" ¡Gracias por registrarte! Bienvenido " + Usuario);
+        
+    }
 }
